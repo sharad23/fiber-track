f8b9de1c5576dcf49c7b75c7591bab1994213343
Sharad Baidya <baidyasharad@gmail.com>
2016-03-01 16:28:15 +0545

    forget password complete

--- config/mail.php
+++ config/mail.php
@@ -54,7 +54,7 @@ return [
 	|
 	*/
 
-	'from' => ['address' => null, 'name' => null],
+	'from' => ['address' => 'baidyasharad@gmail.com', 'name' => 'Sharad Baidya'],
 
 	/*
 	|--------------------------------------------------------------------------
